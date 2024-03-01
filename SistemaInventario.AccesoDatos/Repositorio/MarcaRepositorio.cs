3dc84c5d99948b93453ef1a6de78b9d8b92bee1c
RodrigoRuiz <farid.ruiz2000@gmail.com>
2024-03-01 16:42:56 -0300

    CRUD de marca

---
+++ SistemaInventario.AccesoDatos/Repositorio/MarcaRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using SistemaInventario.AccesoDatos.Data;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio
+{
+    public class MarcaRepositorio : Repositorio<Marca>, IMarcaRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MarcaRepositorio(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Actualizar(Marca marca)
+        {
+            var bodegaBD = _db.Marcas.FirstOrDefault(b => b.Id == marca.Id);
+            if (bodegaBD != null)
+            {
+                bodegaBD.Nombre = marca.Nombre;
+                bodegaBD.Descripcion = marca.Descripcion;
+                bodegaBD.Estado = marca.Estado;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
