a064574ffd7dabfea26953562fa0eacde58ea967
RodrigoRuiz <farid.ruiz2000@gmail.com>
2024-03-13 12:55:39 -0300

    inventario

--- SistemaInventario.AccesoDatos/Repositorio/UnidadTrabajo.cs
+++ SistemaInventario.AccesoDatos/Repositorio/UnidadTrabajo.cs
@@ -21,6 +21,14 @@ namespace SistemaInventario.AccesoDatos.Repositorio
 
         public IUsuarioAplicacionRepositorio UsuarioAplicacion { get; private set; }
 
+        public IBodegaProductoRepositorio BodegaProducto { get; private set; }
+
+        public IInventarioRepositorio Inventario { get; private set; }
+
+        public IInventarioDetalleRepositorio InventarioDetalle { get; private set; }
+
+        public IKardexInventarioRepositorio KardexInventario { get; private set; }
+
         public UnidadTrabajo(ApplicationDbContext db)
         {
             _db = db;
@@ -29,6 +37,10 @@ namespace SistemaInventario.AccesoDatos.Repositorio
             Marca = new MarcaRepositorio(_db);
             Producto = new ProductoRepositorio(_db);
             UsuarioAplicacion = new UsuarioAplicacionRepositorio(_db);
+            BodegaProducto = new BodegaProductoRepositorio(_db);
+            Inventario = new InventarioRepositorio(_db);
+            InventarioDetalle = new InventarioDetalleRepositorio(_db);
+            KardexInventario = new KardexInventarioRepositorio(_db);
         }
 
         public void Dispose()
