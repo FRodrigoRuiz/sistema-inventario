f6c8eef8458fe7328c39fc5b0c3526c036d9ec72
RodrigoRuiz <farid.ruiz2000@gmail.com>
2024-03-14 16:53:13 -0300

    compania

--- SistemaInventario.AccesoDatos/Repositorio/UnidadTrabajo.cs
+++ SistemaInventario.AccesoDatos/Repositorio/UnidadTrabajo.cs
@@ -29,6 +29,8 @@ namespace SistemaInventario.AccesoDatos.Repositorio
 
         public IKardexInventarioRepositorio KardexInventario { get; private set; }
 
+        public ICompaniaRepositorio Compania { get; private set; }
+
         public UnidadTrabajo(ApplicationDbContext db)
         {
             _db = db;
@@ -41,6 +43,7 @@ namespace SistemaInventario.AccesoDatos.Repositorio
             Inventario = new InventarioRepositorio(_db);
             InventarioDetalle = new InventarioDetalleRepositorio(_db);
             KardexInventario = new KardexInventarioRepositorio(_db);
+            Compania = new CompaniaRepositorio(_db);
         }
 
         public void Dispose()
